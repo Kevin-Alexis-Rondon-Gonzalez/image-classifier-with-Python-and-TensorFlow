17649509b26cb974fc331478d96e15b7d7af183b
Kevin Alexis Rondon Gonzalez <kevinalexisrondongonzalez@gmail.com>
2025-05-14 16:48:00 -0400

    Importing libraries and metadata to the project

--- Image_classifier.py
+++ Image_classifier.py
@@ -1,4 +1,10 @@
 import tensorflow as tf 
-import tensorflow_datasets as tfds  
+import tensorflow_datasets as tfds  #Data to training 
 
 datos, metadatos = tfds.load('fashion_mnist', as_supervised = True, with_info = True)
+
+datos_entrenamiento, datos_pruebas = datos['train'], datos ['test']
+
+nombres_clases = metadatos.features['label'].names
+
+#print(nombres_clases)
